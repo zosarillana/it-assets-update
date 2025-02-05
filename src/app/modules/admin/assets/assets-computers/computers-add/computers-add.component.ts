172b6f00a612d7ad084492b5627efa70e44a32cb
zosarillana <zhou991818@gmail.com>
2025-02-05 17:00:57 +0800

    ui enhancement

--- src/app/modules/admin/assets/assets-computers/computers-add/computers-add.component.ts
+++ src/app/modules/admin/assets/assets-computers/computers-add/computers-add.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Asset {
+    id: number;
+    type: string;
+    date_acquired: string;
+    asset_barcode: string;
+    brand: string;
+    model: string;
+}
 @Component({
     selector: 'app-computers-add',
     templateUrl: './computers-add.component.html',
@@ -34,39 +42,92 @@ export class ComputersAddComponent implements OnInit {
     }
     addRow() {
         this.items.push({
-          itemType: 'RAM', // Default selection
-          itemCode: '',
-          description: '',
-          remarks: ''
+            itemType: 'RAM', // Default selection
+            itemCode: '',
+            description: '',
+            remarks: '',
         });
-      }
-    
-      removeRow(index: number) {
-        this.items.splice(index, 1);
-      }
+    }
 
-      accessories = [
-        // { type: "USB-C ADAPTER", date_acquired: "2021-06-02", asset_barcode: "000163", brand: "APPLE", model: "A2164" },
-        // { type: "KEYBOARD USB", date_acquired: "2014-01-11", asset_barcode: "01-01-03-000075-03", brand: "DELL", model: "KB216T" },
-        // { type: "MOUSE USB", date_acquired: "2014-01-11", asset_barcode: "01-01-03-000075-04", brand: "DELL", model: "MSIII-T" },
-        // { type: "MONITOR", date_acquired: "2023-02-14", asset_barcode: "01-01-09-004268-02", brand: "N-VISION", model: "N2755" },
-        // { type: "MONITOR", date_acquired: "2018-06-21", asset_barcode: "01-26-13-000024-02", brand: "ACER", model: "K22HQL" },
-        // { type: "KEYBOARD USB", date_acquired: "2018-06-21", asset_barcode: "01-26-13-000024-03", brand: "MICROPACK", model: "KM-2010" },
-        // { type: "MOUSE USB", date_acquired: "2018-06-21", asset_barcode: "01-26-13-000024-04", brand: "MICROPACK", model: "KM-2010" },
-        // { type: "UPS", date_acquired: "2018-06-21", asset_barcode: "01-26-13-000025-01", brand: "APC", model: "BX625CI-MS" },
-        // { type: "BAG", date_acquired: "2023-03-14", asset_barcode: "01-01-02-004278-03", brand: "LENOVO", model: "NULL" },
-        // { type: "AC ADAPTER", date_acquired: "2020-07-23", asset_barcode: "01-26-13-000051-02", brand: "HP", model: "NULL" },
-        // { type: "AC ADAPTER", date_acquired: "2021-11-26", asset_barcode: "04-01-00-000299-02", brand: "LENOVO", model: "ADLX65CLGU2A" },
-        // { type: "MOUSE WIRELESS", date_acquired: "2021-11-26", asset_barcode: "04-01-00-000299-03", brand: "A4TECH", model: "G3-300N" },
-        // { type: "BAG BACKPACK", date_acquired: "2021-11-26", asset_barcode: "04-01-00-000299-04", brand: "LENOVO", model: "NULL" }
-      ];
-      
-      addAccessoryRow() {
-        this.accessories.push({ type: "", date_acquired: "", asset_barcode: "", brand: "", model: "" });
-      }
-      
-      removeAccessoryRow(index: number) {
-        this.accessories.splice(index, 1);
-      }
-      
+    removeRow(index: number) {
+        this.items.splice(index, 1);
+    }
+
+    assets: Array<{
+        assetId: number;
+        date_acquired: string;
+        asset_barcode: string;
+        brand: string;
+        model: string;
+    }> = [];
+
+    assetList = [
+        {
+            id : 1,
+            type: 'USB-C ADAPTER',
+            date_acquired: '2021-06-02',
+            asset_barcode: '000163',
+            brand: 'APPLE',
+            model: 'A2164',
+        },
+        {
+            id: 2,
+            type: 'KEYBOARD USB',
+            date_acquired: '2014-01-11',
+            asset_barcode: '01-01-03-000075-03',
+            brand: 'DELL',
+            model: 'KB216T',
+        },
+        {
+            id: 3,
+            type: 'MOUSE USB',
+            date_acquired: '2014-01-11',
+            asset_barcode: '01-01-03-000075-04',
+            brand: 'DELL',
+            model: 'MSIII-T',
+        },
+        {
+            id: 4,
+            type: 'MONITOR',
+            date_acquired: '2023-02-14',
+            asset_barcode: '01-01-09-004268-02',
+            brand: 'N-VISION',
+            model: 'N2755',
+        },
+        {
+            id: 5,
+            type: 'UPS',
+            date_acquired: '2018-06-21',
+            asset_barcode: '01-26-13-000025-01',
+            brand: 'APC',
+            model: 'BX625CI-MS',
+        },     
+    ];
+
+    // Update fields when asset is selected
+    onAssetSelect(selectedId: string, index: number) {
+        const selectedAsset = this.assetList.find(asset => asset.id === Number(selectedId));
+        if (selectedAsset) {
+            this.assets[index] = {
+                assetId: selectedAsset.id,
+                date_acquired: selectedAsset.date_acquired,
+                asset_barcode: selectedAsset.asset_barcode,
+                brand: selectedAsset.brand,
+                model: selectedAsset.model
+            };
+        }
+    }
+    addAccessoryRow() {
+        this.assets.push({
+            assetId: 0,
+            date_acquired: '',
+            asset_barcode: '',
+            brand: '',
+            model: ''
+        });
+    }
+
+    removeAccessoryRow(index: number) {
+        this.assets.splice(index, 1);
+    }
 }
