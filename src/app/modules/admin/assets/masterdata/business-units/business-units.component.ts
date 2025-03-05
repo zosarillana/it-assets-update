c980a812e9f400d195e4286897c2e59c0b041836
zosarillana <zhou991818@gmail.com>
2025-03-05 13:32:29 +0800

    --added business units

---
+++ src/app/modules/admin/assets/masterdata/business-units/business-units.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-units',
+  templateUrl: './business-units.component.html',
+  styleUrls: ['./business-units.component.scss']
+})
+export class BusinessUnitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
