30d664201d5675128f33336d4223374e4ffaca3c
zosarillana <zhou991818@gmail.com>
2025-05-22 16:09:25 +0800

    add deffective status and option for pullout, updated displays and added loader for unloaded networks

---
+++ src/app/core/loader/loader.interceptor.ts
@@ -0,0 +1,24 @@
+// loader.interceptor.ts
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoaderService } from 'app/services/loader/loader.service';
+import { finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+  constructor(private loaderService: LoaderService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.loaderService.show();
+    return next.handle(req).pipe(finalize(() => this.loaderService.hide()));
+  }
+}
